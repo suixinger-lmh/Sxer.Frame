a0a29b0f2101aeeb8a5f28e898b2005cdb926ff3
Sxer_lmh <308457938@qq.com>
2022-10-23 19:38:56 +0800

    框架Editor工具

---
+++ Editor/FrameTools/SxerFrameConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sxer.Frame.Editor
+{
+    public static class SxerFrameConfig
+    {
+        public const string SxerFrameRoot = "Assets/SxerFrame";
+        public const string EditorPath_DataOutPutFile = "Assets/Sxer_GetAll_DataOutputFile/";
+        public const string SceneName = "SxerFrameStart.unity";
+        public const string Res_framePrefab = "Sxer_FramePrefab/Sxer_Frame";
+    }
+}
+
---
+++ Runtime/Scripts/Base/Frame.Config.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sxer.Frame
+{
+    public partial class Frame
+    {
+
+
+    }
+
+}
