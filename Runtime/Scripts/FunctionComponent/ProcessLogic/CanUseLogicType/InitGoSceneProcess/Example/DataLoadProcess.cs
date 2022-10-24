e55b798d8efa1ee37ecf1fe2da927816ddedc211
Sxer_lmh <308457938@qq.com>
2022-10-24 08:56:47 +0800

    增加逻辑流程模板

---
+++ Runtime/Scripts/FunctionComponent/ProcessLogic/CanUseLogicType/InitGoSceneProcess/Example/DataLoadProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Sxer.Frame.Func.Process.InitGoSceneProcess
+{
+    public class DataLoadProcess : MonoBehaviour, IBaseInitTemplate
+    {
+        public string afterLoadedScene;
+
+        public void Init()
+        {
+            //////做进入场景前的初始化操作
+            //////
+
+            ///进入场景
+            SceneManager.LoadScene(afterLoadedScene);
+        }
+        void OnDestroy()
+        {
+        }
+
+        void OnApplicationQuit()
+        {
+        }
+    }
+}
+
