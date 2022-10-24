14d5b372b1b296f5e3d0b44e2c0d82f35232b835
Sxer_lmh <308457938@qq.com>
2022-10-24 08:56:47 +0800

    增加逻辑流程模板

---
+++ Assets/com.sxer.frame/Editor/ProcessTools/ProcessLogicEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Sxer.Frame.Editor
+{
+    public class ProcessLogicEditor : UnityEditor.Editor
+    {
+        //创建模板脚本
+        [MenuItem("Sxer/Frame/ProcessLogic/SkipScene:CreateTemplate")]
+        public static void CreateSkipSceneTemplate()
+        {
+            //TextAsset textAsset = 
+        }
+
+
+
+
+
+    }
+}
+
