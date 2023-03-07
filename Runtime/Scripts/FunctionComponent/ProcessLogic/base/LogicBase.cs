62027b77fb05343e6fc43d52f56f7f201e35260e
Sxer_lmh <308457938@qq.com>
2023-03-07 08:57:10 +0800

    修改流程逻辑

--- Runtime/Scripts/FunctionComponent/ProcessLogic/base/LogicBase.cs
+++ Runtime/Scripts/FunctionComponent/ProcessLogic/base/LogicBase.cs
@@ -11,6 +11,7 @@ namespace Sxer.Frame.Func.Process
         public virtual void ProcessStart() { }//开始
         public virtual void ProcessPause() { }//暂停
         public virtual void ProcessUnPause() { }//取消暂停
+        public virtual void ProcessUpdate() { }//持续执行  //目前update操作单独放在每个逻辑类型里的Update里执行，后期可集成在管理类中
         public virtual void ProcessEnd() { }//结束
       //  public virtual void ProcessChange(XMLProcessBase) { }
     }
